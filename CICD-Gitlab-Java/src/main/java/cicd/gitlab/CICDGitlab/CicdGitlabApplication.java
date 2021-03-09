8a97486110243baaf79efc93eb527fca6369dc8e
Havyx <everluca@outlook.com>
2021-03-09 11:28:00 -0300

    :sparkles: java ci cd build

---
+++ CICD-Gitlab-Java/src/main/java/cicd/gitlab/CICDGitlab/CicdGitlabApplication.java
@@ -0,0 +1,13 @@
+package cicd.gitlab.CICDGitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdGitlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdGitlabApplication.class, args);
+	}
+
+}
---
+++ CICD-Gitlab-Java/src/test/java/cicd/gitlab/CICDGitlab/CicdGitlabApplicationTests.java
@@ -0,0 +1,13 @@
+package cicd.gitlab.CICDGitlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdGitlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
